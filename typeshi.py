345927034ba01de812fd00fe36df2c41d473713f
PietroMatheus <140650026+PietroMatheus@users.noreply.github.com>
2025-03-27 14:50:09 -0300

    Update typeshi.py

--- typeshi.py
+++ typeshi.py
@@ -8,10 +8,14 @@ lista_calculos = []
 @app.route("/soma", methods=["POST"])
 def somar():
     dados_receba = request.get_json()
-    numero1 = dados_receba['numero1']
-    numero2 = dados_receba['numero2']
+    try:
+        numero1 = dados_receba['numero1']
+        numero2 = dados_receba['numero2']
+    except:
+        return {"Message":"Erro"}, 400
     resultado = numero1 + numero2
     lista_calculos.append({
+        'id': str(uuid.uuid4()),
         'numero1': numero1,
         'numero2': numero2,
         'resultado': resultado
@@ -22,14 +26,36 @@ def somar():
 def calculos():
     return lista_calculos
 
-# @app.route("/", methods=["PUT"])
-# def metodo_put():
-#     dados_receba = request.get_json()
-#     coisalegal.append({"nome": dados_receba["nome"]})
-#     return {"message": "deu boa"}
+@app.route("/deletar/<id>", methods=["DELETE"])
+def metodo_delete(id):
+    global lista_calculos
+    lista_calculos = [d for d in lista_calculos if d.get('id') != id]
 
-# @app.route("/", methods=["DELETE"])
-# def metodo_delete():
-#     dados_deletar = request.get_json()
-#     coisalegal.remove({"nome": dados_deletar["nome"]})
-#     return {"message": "deu boa"}
+    return {}, 200
+
+@app.route("/editar/<id>", methods=["PUT"])
+def editar_item(id):
+    global lista_calculos
+    dados_receba = request.get_json()
+    try:
+        numero1 = dados_receba['numero1']
+        numero2 = dados_receba['numero2']
+    except:
+        return {"Message":"Erro"}, 400
+
+    lista_temp = []
+    for calculo in lista_calculos:
+        if calculo['id'] == id:
+            resultado = numero1 = numero2
+            lista_temp.append({
+                'id': id,
+                'numero1': numero1,
+                'numero2': numero2,
+                'resultado': resultado
+            })
+            return {}, 200
+        else:
+            return {}, 404
+
+if __name__ == "__main__":
+    app.run()
